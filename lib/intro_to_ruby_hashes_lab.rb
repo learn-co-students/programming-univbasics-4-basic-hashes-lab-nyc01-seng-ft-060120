5ef206c9d4c5800c1727d52ee2eb157f9b1b7db1
Mark Camilleri <markcamilleri10@gmail.com>
2020-05-08 17:43:53 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,23 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  full_name = {
+    first_name: "Mark",
+    last_name: "Camilleri"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = {
+    :name => "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  new_hash = {
+    id: number
+  }
 end
